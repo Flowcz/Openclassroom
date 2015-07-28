fd0205f6e50229a7d7a883b4dbd5a6c478820324
LaurentSta <lostaelens@gmail.Com>
2015-07-28 20:41:13 +0200

    Update 2.0_EcrireUneBddVariable.php

--- 2.0_EcrireUneBddVariable.php
+++ 2.0_EcrireUneBddVariable.php
@@ -1,1 +1,15 @@
 
+<!-- Ecrire Bdd -->
+ <?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+// On ajoute une entrée dans la table testopenclass
+$bdd->exec('INSERT INTO testopenclass(nom, texte) VALUES(\'Ariane\', \'Bonjour je mappel Ariane\')');
+echo 'Le jeu a bien été ajouté !';
+?>
